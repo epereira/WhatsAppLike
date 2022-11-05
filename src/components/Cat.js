9a07c2739d3cbae492efb0fdb09c8d3980a4e92d
Emmanuel PEREIRA <epereira777@gmail.com>
2022-11-05 23:36:54 +0100

    Add sidebar menu and switch theme dark/light

---
+++ src/components/Cat.js
@@ -0,0 +1,9 @@
+import { Avatar } from '@mui/material'
+
+const Cat = () => {
+  return (
+    <Avatar src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk6qxRGXu9_Mou7APQCryC7T3F7s_YXmCSWQ&usqp=CAU' />
+  )
+}
+
+export default Cat
