93551ac63201344bd751f722bdb9472c37f3fffb
Emmanuel PEREIRA <epereira777@gmail.com>
2022-11-08 20:06:40 +0100

    Chat added

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,17 +1,26 @@
+import { useTheme } from "@mui/material/styles";
 import { Box, Stack } from "@mui/system";
 import React from "react";
 import Conversation from "../../components/Conversation";
 import Chats from "./Chats";
 
-
 const GeneralApp = () => {
-
+  const theme = useTheme();
   return (
-    <Stack direction="row" sx={{width: '100vw'}}>
-        <Chats />
-        <Box sx={{height: '100%', width: "calc(100vw-420px)", backgroundColor: "#fff"}}>
-            <Conversation />
-        </Box>
+    <Stack direction="row" sx={{ width: "100vw" }}>
+      <Chats />
+      <Box
+        sx={{
+          height: "100%",
+          width: "calc(100vw - 420px)",
+          backgroundColor:
+            theme.palette.mode === "light"
+              ? "#fff"
+              : theme.palette.background.default,
+        }}
+      >
+        <Conversation />
+      </Box>
     </Stack>
   );
 };
